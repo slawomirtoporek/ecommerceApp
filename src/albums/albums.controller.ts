7d48a4ef15ad40aec146fd712cf1ca5f80990af5
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-28 13:21:15 +0200

    Add the getAll and getById endpoint for the albums module

--- src/albums/albums.controller.ts
+++ src/albums/albums.controller.ts
@@ -1,7 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  ParseUUIDPipe,
+  NotFoundException,
+  Get,
+  Param,
+} from '@nestjs/common';
 import { AlbumsService } from './albums.service';
 
 @Controller('albums')
 export class AlbumsController {
   constructor(private albumsService: AlbumsService) {}
+
+  @Get('/')
+  getAll() {
+    return this.albumsService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const album = await this.albumsService.getById(id);
+
+    if (!album) throw new NotFoundException('Album not found');
+
+    return album;
+  }
 }
--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { Album } from '@prisma/client';
 
 @Injectable()
 export class AlbumsService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Album[]> {
+    return this.prismaService.album.findMany({
+      include: {
+        images: true,
+      },
+    });
+  }
+
+  public getById(id: Album['id']): Promise<Album | null> {
+    return this.prismaService.album.findUnique({
+      where: { id },
+      include: {
+        tracks: true,
+        images: true,
+      },
+    });
+  }
 }
