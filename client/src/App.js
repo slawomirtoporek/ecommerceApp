116f2daa26dbcdb3acb53afcdbfefbe9db5f39e8
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-07-30 19:39:54 +0200

    Add routes for subpages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/pages/Home/Home';
+import Album from './components/pages/Album/Album';
+import Cart from './components/pages/Cart/Cart';
+import Order from './components/pages/Order/Order';
+
 const App = () => {
   return (
-    <h2>e-commerceApp</h2>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/album/:id" element={<Album />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/order" element={<Order />} />
+      <Route path="*" element={<Home />} />
+    </Routes>
   );
 }
 
