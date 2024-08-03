e593a831218d5b9a74a742e2377d3ec659a8e066
Sławomir Toporek <toporek.slawomir@gmail.com>
2024-08-03 18:48:39 +0200

    Add static files to public folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,17 +10,33 @@ import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { OrdersModule } from './orders/orders.module';
 import { OrderItemsModule } from './order-items/order-items.module';
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [AlbumsModule, OrdersModule, OrderItemsModule],
+  imports: [
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '../../', 'client', 'build'),
+    // }),
+    AlbumsModule,
+    OrdersModule,
+    OrderItemsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(cors()).forRoutes({
-      path: '*',
-      method: RequestMethod.ALL,
-    });
+    consumer
+      .apply(
+        cors({
+          origin: 'http://localhost:3000',
+          credentials: true,
+        }),
+      )
+      .forRoutes({
+        path: '*',
+        method: RequestMethod.ALL,
+      });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use('/public', express.static('public'));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   await app.enableShutdownHooks();
-  await app.listen(3000);
+  await app.listen(8000);
 }
 bootstrap();
